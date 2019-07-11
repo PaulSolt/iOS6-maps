6301a7b86a73addc09e4b1643274a99b26b88da9
Paul Solt <PaulSolt@gmail.com>
2019-07-11 12:20:31 -0400

    Initial commit

---
+++ iOS6-Quakes/iOS6-Quakes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS6-Quakes
+//
+//  Created by Paul Solt on 7/11/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
